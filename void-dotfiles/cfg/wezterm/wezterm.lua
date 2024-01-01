e4297c2c9a2405f46ff49bc0e6d1efbd52ab2d43
Farah-xyz <faaraah.xyz@gmail.com>
2024-01-01 20:08:30 +0100

    added KeyBidings

--- void-dotfiles/cfg/wezterm/wezterm.lua
+++ void-dotfiles/cfg/wezterm/wezterm.lua
@@ -33,26 +33,66 @@ return {
 		indexed = {[16] = "#F8BD96", [17] = "#F5E0DC"},
 		split = "#161320",
 		visual_bell = "#302D41",
-	},
+   },
 -----------------------------------------------------------
 -- Window Configurations:
 -----------------------------------------------------------
+  window_background_opacity = 1.0,
+  window_decorations = "TITLE | RESIZE",
   window_padding = {
     left = 15,
     right = 15,
     top = 10,
     bottom = 0,
   },
-  window_background_opacity = 1.0,
-  window_decorations = "RESIZE",
 -----------------------------------------------------------
 -- Tab Configurations:
 -----------------------------------------------------------
-  enable_tab_bar = false,
+  enable_tab_bar = true,
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
 -----------------------------------------------------------
 -- ScrollBar Configurations:
 -----------------------------------------------------------
   scrollback_lines = 5000,
   enable_scroll_bar = false,
   check_for_updates = false,
+-----------------------------------------------------------
+-- Keybidings Configurations:
+-----------------------------------------------------------
+  leader = { key="a", mods="CTRL" },
+  disable_default_key_bindings = true,
+  keys = {
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+    { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+    { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+
+    { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
+    { key = "v", mods="SHIFT|CTRL",     action=wezterm.action.PasteFrom 'Clipboard'},
+    { key = "c", mods="SHIFT|CTRL",     action=wezterm.action.CopyTo 'Clipboard'},
+  },
+ set_environment_variables = {},
 }
