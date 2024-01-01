83504b3bd92e80d6b4667024672564f5d48241c7
Farah-xyz <faaraah.xyz@gmail.com>
2024-01-01 17:31:24 +0100

    Less Timeout In swayidle

--- void-dotfiles/cfg/wezterm/wezterm.lua
+++ void-dotfiles/cfg/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 return {
   bidi_enabled = true,
   bidi_direction = "AutoLeftToRight",
-  font_size = 11.5,
+  font_size = 11.0,
   font = wezterm.font("JetBrainsMono Nerd Font"),
   color_scheme = "Gruvbox dark, hard (base16)",
   window_decorations = "RESIZE",
