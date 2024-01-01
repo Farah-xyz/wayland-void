4cf09e57581f283521e36702e5c4ff366dcaf86e
Farah-xyz <faaraah.xyz@gmail.com>
2024-01-01 17:54:44 +0100

    add documentations

--- void-dotfiles/cfg/wezterm/wezterm.lua
+++ void-dotfiles/cfg/wezterm/wezterm.lua
@@ -1,19 +1,57 @@
 local wezterm = require 'wezterm';
 return {
+-----------------------------------------------------------
+-- For Support right to left languages like arabic
+-----------------------------------------------------------
   bidi_enabled = true,
-  bidi_direction = "AutoLeftToRight",
+  bidi_direction = "LeftToRight",
+-----------------------------------------------------------
+-- Fonts Configurations:
+-----------------------------------------------------------
+  font = wezterm.font("JetBrainsMono NF", {weight="Medium", stretch="Normal", style="Normal"}),
   font_size = 11.0,
-  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font("JetBrainsMono NF", {weight="Medium", stretch="Normal", style="Italic"}),
+    },
+    {
+      intensity = "Bold",
+      font = wezterm.font("JetBrainsMono NF", {weight="Bold", stretch="Normal", style="Normal"}),
+    },
+    {
+      intensity = "Bold",
+      italic = true,
+      font = wezterm.font("JetBrainsMono NF", {weight="Bold", stretch="Normal", style="Italic"})
+    },
+  },
+-----------------------------------------------------------
+-- Colorscheme Configurations:
+-----------------------------------------------------------
   color_scheme = "Gruvbox dark, hard (base16)",
-  window_decorations = "RESIZE",
-  window_background_opacity = 1,
+  colors = {
+		indexed = {[16] = "#F8BD96", [17] = "#F5E0DC"},
+		split = "#161320",
+		visual_bell = "#302D41",
+	},
+-----------------------------------------------------------
+-- Window Configurations:
+-----------------------------------------------------------
   window_padding = {
-    left = 5,
-    right = 5,
-    top = 5,
+    left = 15,
+    right = 15,
+    top = 10,
     bottom = 0,
   },
+  window_background_opacity = 1.0,
+  window_decorations = "RESIZE",
+-----------------------------------------------------------
+-- Tab Configurations:
+-----------------------------------------------------------
   enable_tab_bar = false,
+-----------------------------------------------------------
+-- ScrollBar Configurations:
+-----------------------------------------------------------
   scrollback_lines = 5000,
   enable_scroll_bar = false,
   check_for_updates = false,
